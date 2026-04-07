0e58684f30dea115002e299a574250d7b9e03602
IoFilms8149 <battlebot399@gmail.com>
2026-04-07 12:22:29 +1200

    feat: add userinfo command

--- main.py
+++ main.py
@@ -47,18 +47,38 @@ async def help(interaction: discord.Interaction):
         description="Here is a list of all available commands",
         color=discord.Colour.blue()
         )
-    embed.add_field(name="/ping", value="Check the bot's latency.", inline=False)
-    embed.add_field(name="/kick [member] [reason]", value="Kicks a member from the server.", inline=False)
     embed.add_field(name="/ban [member] [reason]", value="Bans a member from the server.", inline=False)
-    embed.add_field(name="/unban [user_id]", value="Unbans an already banned member.", inline=False)
-    embed.add_field(name="/tempban [member] [minutes] [reason]", value="Temporarily bans a member for a set amount of time", inline=False)
     embed.add_field(name="/clear [amount]", value="Deletes a certaina amount of messages.", inline=False)
+    embed.add_field(name="/kick [member] [reason]", value="Kicks a member from the server.", inline=False)
+    embed.add_field(name="/ping", value="Check the bot's latency.", inline=False)
+    embed.add_field(name="/tempban [member] [minutes] [reason]", value="Temporarily bans a member for a set amount of time", inline=False) 
+    embed.add_field(name="/unban [user_id]", value="Unbans an already banned member.", inline=False)
+    embed.add_field(name="userinfo [member]", value="Gets information about a member and their roles.", inline=False)
     await interaction.response.send_message(embed=embed)
 # Ping Command
 @bot.tree.command(name="ping", description="Tells the latency (ms) of the bot")
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message(f'Pong! :ping_pong: {bot.latency * 1000:.2f} ms')
-
+# User Info Command
+@bot.tree.command(name="userinfo", description="Shows information about a member.")
+@app_commands.describe(member="The member you want information about.")
+async def userinfo(interaction: discord.Interaction, member: discord.Member = None):
+    member = member or interaction.user
+    roles = [
+    role.mention for role in reversed(member.roles) 
+    if role.id != interaction.guild.id and role.name != "@everyone" and role.name != "everyone"
+    ]   
+    role_string = ", ".join(roles) if roles else "No roles assigned"
+    embed = discord.Embed(
+        title=f"User Info - {member.display_name}",
+        color=member.color
+    )
+    embed.set_thumbnail(url=member.display_name if not member.avatar else member.avatar.url)
+    embed.add_field(name="ID", value=member.id, inline=False)
+    embed.add_field(name="Top Role", value=member.top_role.mention, inline=False)
+    embed.add_field(name=f"Roles: {len(roles)}", value=role_string, inline=False)
+    embed.set_footer(text=f"Requested by {interaction.user.display_name}")
+    await interaction.response.send_message(embed=embed)
 # Kick command
 @bot.tree.command(name="kick", description="Kicks a member from the server.")
 @app_commands.describe(member="The user to kick", reason="The reason for the kick")
